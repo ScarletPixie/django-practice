9fcb014c25b2458e2ebf4e5947374838ecba3936
paulhenr <voxskynned@gmail.com>
2024-11-29 13:45:41 -0300

    fixed import

--- polls_project/polls/admin.py
+++ polls_project/polls/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+	readonly_fields = ['pk']
+class ChoiceAdmin(admin.ModelAdmin):
+	readonly_fields = ['pk']
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
--- polls_project/polls/models.py
+++ polls_project/polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
@@ -7,6 +9,10 @@ class Question(models.Model):
 
 	def __str__(self):
 		return self.question
+
+	def was_published_recently(self):
+		return self.pub >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
