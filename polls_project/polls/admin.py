6d24e5412a72446ab45580ec7df0c199174d275d
sayori <voxskynned@gmail.com>
2024-12-01 16:57:52 -0300

    added tabular view for question in admin interface

--- polls_project/polls/admin.py
+++ polls_project/polls/admin.py
@@ -3,9 +3,14 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 4
 
 class QuestionAdmin(admin.ModelAdmin):
 	readonly_fields = ['pk']
+	inlines = [ChoiceInline]
+
 class ChoiceAdmin(admin.ModelAdmin):
 	readonly_fields = ['pk']
 
