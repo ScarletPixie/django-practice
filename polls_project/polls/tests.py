6e8c5bf8810eca72fdb588a8c2df68aeaa66145c
paulhenr <voxskynned@gmail.com>
2024-12-02 14:35:55 -0300

    added remaining tests

--- polls_project/polls/tests.py
+++ polls_project/polls/tests.py
@@ -19,8 +19,17 @@ class QuestionModelTests(TestCase):
         """
         time1 = timezone.now() - datetime.timedelta(days = 1)
         time2 = timezone.now() - datetime.timedelta(days = 100)
-        one_day_old_question = Question(pub = time1);
-        very_old_question = Question(pub = time2);
+        one_day_old_question = Question(pub = time1)
+        very_old_question = Question(pub = time2)
         self.assertIs(one_day_old_question.was_published_recently(), False)
         self.assertIs(very_old_question.was_published_recently(), False)
 
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently returns True for questions published within a day
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=50)
+        recent1 = Question(pub = time)
+        recent2 = Question(pub = timezone.now())
+        self.assertIs(recent1.was_published_recently(), True)
+        self.assertIs(recent2.was_published_recently(), True)
