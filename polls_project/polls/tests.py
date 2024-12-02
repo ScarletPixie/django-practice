97bd281dbec6d7c13d555d3691dccabf44db09af
paulhenr <voxskynned@gmail.com>
2024-12-02 16:36:06 -0300

    added tests to index view

--- polls_project/polls/tests.py
+++ polls_project/polls/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
-from .models import Question, Choice
+from .models import Question
 from django.utils import timezone
+from django.test.utils import setup_test_environment, teardown_test_environment
+from django.urls import reverse
 import datetime
 
-# Create your tests here.
+# Question specific tests
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         """
@@ -33,3 +35,49 @@ class QuestionModelTests(TestCase):
         recent2 = Question(pub = timezone.now())
         self.assertIs(recent1.was_published_recently(), True)
         self.assertIs(recent2.was_published_recently(), True)
+
+# view specific tests
+#  helper function
+def create_question(text: str, days: int):
+    """ cretes a new Question object with parameters text and days offset from timezone.now() """
+    return Question.objects.create(pub=timezone.now() + datetime.timedelta(days=days), question=text)
+
+class QuestionIndexViewTests(TestCase):
+
+    def test_no_questions(self):
+        """
+        shows 'no polls available' if there are no questions or no questions with a past date
+        """
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No polls available')
+        self.assertQuerySetEqual(response.context['question_list'], [])
+
+    def test_past_questions(self):
+        question = create_question(text='dummy question 0', days=-10)
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'dummy question 0')
+        self.assertQuerySetEqual(response.context['question_list'], [question])
+
+
+    def test_only_shows_5_recent_questions(self):
+        """
+        Only the 5 most recent questions excluding future questions are sent to index
+        """
+        question_num = 10
+        questions = []
+        for i in range(question_num):
+            questions.append(create_question(text=f"dummy question {i}", days=-i))
+
+        _ = create_question(text="back to the future", days=1000)
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+
+        for i in range(question_num):
+            if (i < 5):
+                self.assertContains(response, f"dummy question {i}")
+            else:
+                self.assertNotContains(response, f"dummy question {i}")
+
+        self.assertQuerySetEqual(response.context['question_list'], questions[:5])
